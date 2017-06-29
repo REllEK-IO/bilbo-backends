f766138ed869faa1c4273a6fb29570454f51a978
contour66 <zamir.thind@gmail.com>
2017-06-28 18:09:49 -0700

    stuff

--- server/models/yummlyQuery.js
+++ server/models/yummlyQuery.js
@@ -1,7 +1,7 @@
 // ============ URL ================
 var app_id= "_app_id=966158dd";
 var app_key = "&_app_key=1a26a81b5d589e14c6857ca6160e5df4";
-var cuisine = ["chinese", ;
+var cuisine = ["chinese", "indian" ;
 var url = "http://api.yummly.com/v1/api/recipes?" + app_id + app_key + "&q=" + cuisine;
 
  queryYummly: function (){
@@ -14,7 +14,7 @@ var url = "http://api.yummly.com/v1/api/recipes?" + app_id + app_key + "&q=" + c
 			var fourth = this.randomArray[3];
 			var fifth = this.randomArray[4];
 
-			// $.when( ajax1(), ajax2(), ajax3(), ajax4(), ajax5()).done(function(a1, a2, a3, a4, a5){
+			 $.when( ajax1(), ajax2(), ajax3(), ajax4(), ajax5()).done(function(a1, a2, a3, a4, a5){
 
 
 				
