f19bb2270c2e731fe16cb0332799306f5547eccb
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-07-02 17:31:31 -0700

    styling

--- client/src/components/children/Area.js
+++ client/src/components/children/Area.js
@@ -8,27 +8,29 @@ class Area extends React.Component {
     // This super(props) line lets us access our parents properties as props.
     super(props);
     this.state = {
-      priceLevel: []
+      range: 0
     }
   }
     render() {
         return (
-                  <div id="dollar-signs">
-                    <input type="checkbox" name="close" id="walk" className="input-hidden" />
-                    <label htmlFor="walk">
-                      <img id = "singleimg" src="https://d30y9cdsu7xlg0.cloudfront.net/png/15371-200.png"/>
-                    </label>
+          <form id="area">
+            <div id="dollar-signs">
+              <input type="checkbox" name="close" id="walk" className="input-hidden" value="1000"/>
+              <label htmlFor="walk">
+                <img id = "singleimg" src="https://d30y9cdsu7xlg0.cloudfront.net/png/15371-200.png"/>
+              </label>
 
-                    <input type="checkbox" name="near" id="bike" className="input-hidden" />
-                    <label htmlFor="bike">
-                      <img id = "doubleimg" src="https://cdn3.iconfinder.com/data/icons/bikecons/512/bikecons_tri-512.png"/>
-                    </label>
+              <input type="checkbox" name="near" id="bike" className="input-hidden" value="2000"/>
+              <label htmlFor="bike">
+                <img id = "doubleimg" src="https://cdn3.iconfinder.com/data/icons/bikecons/512/bikecons_tri-512.png"/>
+              </label>
 
-                    <input type="checkbox" name="far" id="car" className="input-hidden" />
-                    <label htmlFor="car">
-                      <img id = "tripleimg" src="http://www.iconsdb.com/icons/preview/black/car-xxl.png"/>
-                    </label>
-                  </div>
+              <input type="checkbox" name="far" id="car" className="input-hidden" value="3000"/>
+              <label htmlFor="car">
+                <img id = "tripleimg" src="http://www.iconsdb.com/icons/preview/black/car-xxl.png"/>
+              </label>
+            </div>
+          </form>
         );
     }
 };
