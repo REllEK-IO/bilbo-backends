31c9e1d9da4da9fdd833dfced48ee49ed3e4a4c0
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-07-02 20:52:16 -0700

    state updates for range

--- client/src/components/children/Area.js
+++ client/src/components/children/Area.js
@@ -1,33 +1,40 @@
 // update state for range and price value
-// make it so that the buttons hpdate those variables
+// make it so that the buttons update those variables
 
 import React from "react";
 
 class Area extends React.Component {
     constructor(props) {
-    // This super(props) line lets us access our parents properties as props.
     super(props);
     this.state = {
-      range: 0
-    }
+      range: 3000
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(event) {
+    var newState = {};
+    newState[event.target.name] = event.target.value;
+    this.setState(newState);
+    console.log("Previous range value:", this.state.range);
+  }
+
     render() {
         return (
           <form id="area">
             <div id="dollar-signs">
-              <input type="checkbox" name="close" id="walk" className="input-hidden" value="1000"/>
+              <input type="radio" name="range" id="walk" className="input-hidden" value={1000} onChange={this.handleChange}/>
               <label htmlFor="walk">
-                <img id = "singleimg" src="https://d30y9cdsu7xlg0.cloudfront.net/png/15371-200.png"/>
+                <img id = "singleimg" alt="Walking Distance" src="https://d30y9cdsu7xlg0.cloudfront.net/png/15371-200.png"/>
               </label>
 
-              <input type="checkbox" name="near" id="bike" className="input-hidden" value="2000"/>
+              <input type="radio" name="range" id="bike" className="input-hidden" value={2000} onChange={this.handleChange}/>
               <label htmlFor="bike">
-                <img id = "doubleimg" src="https://cdn3.iconfinder.com/data/icons/bikecons/512/bikecons_tri-512.png"/>
+                <img id = "doubleimg" alt="Biking Distance" src="https://cdn3.iconfinder.com/data/icons/bikecons/512/bikecons_tri-512.png"/>
               </label>
 
-              <input type="checkbox" name="far" id="car" className="input-hidden" value="3000"/>
+              <input type="radio" name="range" id="car" className="input-hidden" value={3000} onChange={this.handleChange}/>
               <label htmlFor="car">
-                <img id = "tripleimg" src="http://www.iconsdb.com/icons/preview/black/car-xxl.png"/>
+                <img id = "tripleimg" alt="Driving Distance" src="http://i.imgur.com/CqXp2vO.png"/>
               </label>
             </div>
           </form>
