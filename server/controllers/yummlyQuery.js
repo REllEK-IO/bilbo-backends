1abd82ccb66c253c3e5c3f9b00d1995fe1bfdfad
contour66 <zamir.thind@gmail.com>
2017-07-05 23:44:09 -0700

    changed query call and updated some other stuff

--- server/controllers/yummlyQuery.js
+++ server/controllers/yummlyQuery.js
@@ -1,9 +1,10 @@
 
 //======================= Dependencies ============================================
 
-var axios = require("axios");
+var request = require("request");
 var _ = require("underscore");
-
+var Bluebird = require("bluebird");
+var rp = require("request-promise");
 // ========================== URL =================================================
 
 var app_id= "_app_id=966158dd";
@@ -26,6 +27,7 @@ var queryWord5 = require("./wordGenerator").finalArray[4];
 var formatLog = require("./wordGenerator").formatLog;
 
 //========================= Variables ==============================================
+		// var requestAsync = promisify(request);
 
 var recipeArray = [];
 var finalRecipeArray = [];
@@ -37,66 +39,69 @@ var processQuery = {
 	wordArray: [],
 	totalArray: [],
 
-	getWordOne: function(){
-  		return axios.get(url + queryWord1);
-	},
-	
-	getWordTwo: function(){
-  		return axios.get(url + queryWord2);
-	},
-	
-	getWordThree: function(){
-  		return axios.get(url + queryWord3);
-	},
-	
-	getWordFour: function(){
-  		return axios.get(url + queryWord4);
-	},
-	
-	getWordFive: function(){
-  		return axios.get(url + queryWord5);
-	},
-
 	getRecipes: function (word){
 		console.log("Query URL:" + url + "\n" + line);
 		formatLog(finalArray);
 		var self = this;
-		axios.all([this.getWordOne(), this.getWordTwo(), this.getWordThree(), this.getWordFour(), this.getWordFive()])
-		.then(axios.spread(function (getOne, getTwo, getThree, getFour, getFive){ 
-		 	var recipeLength1= getOne.data.matches.length;
-		 	var recipeLength2= getTwo.data.matches.length;
-		 	var recipeLength3= getThree.data.matches.length;
-		 	var recipeLength4= getFour.data.matches.length;
-		 	var recipeLength5= getFive.data.matches.length;
-		 	console.log("Recipe Array 1 Length: " + recipeLength1 + "\n");
-		 	console.log("Recipe Array 2 Length: " + recipeLength2 + "\n");
-		 	console.log("Recipe Array 3 Length: " + recipeLength3 + "\n");
-		 	console.log("Recipe Array 4 Length: " + recipeLength4 + "\n");
-		 	console.log("Recipe Array 5 Length: " + recipeLength5 + "\n");
-			for (var i = 0 ; i < recipeLength1; i++){
-				var recipeMatchOne = getOne.data.matches[i].recipeName;
+		var query1 = rp(url + queryWord1);
+		var query2 = rp(url + queryWord2);
+		var query3 = rp(url + queryWord3);
+		var query4 = rp(url + queryWord4);
+		var query5 = rp(url + queryWord5);
+		Bluebird.all([query1, query2, query3, query4, query5])
+    	.spread(function (res1, res2, res3, res4, res5){  
+    		var responseParsed1 = JSON.parse(res1);
+    		var responseParsed2 = JSON.parse(res2);
+			var responseParsed3 = JSON.parse(res3);
+			var responseParsed4 = JSON.parse(res4);
+			var responseParsed5 = JSON.parse(res5);
+    		var search1 = Object.keys(responseParsed1["matches"]).map(function(key){
+    			return responseParsed1["matches"][key];
+    		});
+    		var search2 = Object.keys(responseParsed2["matches"]).map(function(key){
+    			return responseParsed2["matches"][key];
+    		});
+    		var search3 =Object.keys(responseParsed3["matches"]).map(function(key){
+    			return responseParsed3["matches"][key];
+    		});
+    		var search4 =Object.keys(responseParsed4["matches"]).map(function(key){
+    			return responseParsed4["matches"][key];
+    		});
+    		var search5 =Object.keys(responseParsed5["matches"]).map(function(key){
+    			return responseParsed5["matches"][key];
+    		});
+	        console.log("Recipe Array 1 Length: " + search1.length + "\n");
+			console.log("Recipe Array 2 Length: " + search2.length+ "\n");
+			console.log("Recipe Array 3 Length: " + search3.length + "\n");
+		 	console.log("Recipe Array 4 Length: " + search4.length + "\n");
+			console.log("Recipe Array 5 Length: " + search5.length+ "\n");
+			for (var i = 0 ; i < search1.length; i++){
+				var recipeMatchOne = search1[i].recipeName;
 			 	recipeArray.push(recipeMatchOne);
-			}
-			for (var i = 0 ; i < recipeLength2; i++){
-				var recipeMatchTwo = getTwo.data.matches[i].recipeName;
+		 	}
+			for (var i = 0 ; i < search2.length; i++){
+				var recipeMatchTwo = search2[i].recipeName;
 			 	recipeArray.push(recipeMatchTwo);
 			}
-			for (var i = 0 ; i < recipeLength3; i++){
-				var recipeMatchThree = getThree.data.matches[i].recipeName;
+			for (var i = 0 ; i < search3.length; i++){
+				var recipeMatchThree = search3[i].recipeName;
 			 	recipeArray.push(recipeMatchThree);
 			}
-			for (var i = 0 ; i < recipeLength4; i++){
-				var recipeMatchFour = getFour.data.matches[i].recipeName;
+			for (var i = 0 ; i < search4.length; i++){
+				var recipeMatchFour = search4[i].recipeName;
 			 	recipeArray.push(recipeMatchFour);
 			}
-			for (var i = 0 ; i < recipeLength5; i++){
-				var recipeMatchFive = getFive.data.matches[i].recipeName;
+			for (var i = 0 ; i < search5.length; i++){
+				var recipeMatchFive = search5[i].recipeName;
 			 	recipeArray.push(recipeMatchFive);
-			}
-		 	console.log( "Search Results: " + "\n" );
-		 	formatLog (recipeArray); 
+		 	}
+		  	console.log( "Search Results: " + "\n" );
+		  	formatLog (recipeArray); 
+		  	
+		
 			self.reduceArray(recipeArray);
-		}));
+		// }));
+		});
 	},
 
 	reduceArray: function (array){
@@ -132,6 +137,8 @@ var processQuery = {
 		return this.finalArray;
 	},
 
-}
+ }
 
-processQuery.getRecipes();
+ processQuery.getRecipes();
+
+module.exports = processQuery;
