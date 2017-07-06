8bbb6414bc03016bd8a183c8cf8a622793ccceaa
Andrew Gardner <andrewgardner@andrews-air-2.dynamic.ucsd.edu>
2017-07-05 20:42:05 -0700

    added reload button in cloud

--- client/src/components/WordCloud.js
+++ client/src/components/WordCloud.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TagCloud } from "react-tagcloud";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const options = {
   luminosity: 'light',
@@ -24,6 +25,9 @@ class WordCloud extends Component{
                 tags={this.state.wordCloudValue}
                 style={{width: 500, textAlign: 'center'}}
                 className="myTagCloud center-block text-center" />
+                <center>
+                  <button id="reload"><img id="reload-pic" src="http://downloadicons.net/sites/default/files/anticlockwise-arrow-icon-94745.png"></img></button>
+                </center>
       </div>
     );
   }
