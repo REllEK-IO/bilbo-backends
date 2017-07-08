9b1d072a96777e95937cfca516d98f3ac61b6f42
individualist-null <micahtk@protonmail.com>
2017-07-07 21:46:18 -0700

    Testing some stuff

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ mongoose.Promise = Promise;
 
 console.log("run");
 
-mongoose.connect("mongodb://hostman:hostmanbestman1@ds151662.mlab.com:51662/heroku_xqq82l40", options);
+mongoose.connect("mongodb://localhost:27017", options);
 
 var db = mongoose.connection;
 db.on("error", function (error) {
